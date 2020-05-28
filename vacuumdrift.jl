87695fd92c1760d77481ad5db4245954905d5cbc
hervasa2 <hervasa2@live.unc.edu>
2020-05-28 11:29:28 -0400

    vacuum drift

--- vacuumdrift.jl
+++ vacuumdrift.jl
@@ -1,4 +1,26 @@
-function driftcharge!(path, charge)
+function getindex(pos, fieldaxes)
+    x = fieldaxes[1]
+    y = fieldaxes[3]
+    difx = abs.(x .- pos[1])
+    dify = abs.(y .- pos[2])
+    i = sortperm(difx)[1]
+    j = sortperm(dify)[1]
+    return [i,j]
+end
+
+function getEfield(pos , simulation)
+    index = getindex(pos, simulation.electric_field.grid.axes)
+    i,j = index[1], index[2]
+    return [simulation.electric_field[i,1,j][1], simulation.electric_field[i,1,j][3]]
+end
+
+function getWpot(pos, simulation)
+    index = getindex(pos, simulation.weighting_potentials[1].grid.axes)
+    i,j = index[1], index[2]
+    return simulation.weighting_potentials[1][i,1,j]
+end
+
+function driftcharge!(path, charge, simulation)
     q = 0
     e = 1.602176634e-19
     m = 9.1093837015e-31
@@ -11,7 +33,7 @@ function driftcharge!(path, charge)
     end
     vel = zeros(size(path))
     for i in 2:size(path)[1]
-        a = q*getEfield(path[i-1,:])/m
+        a = q*getEfield(path[i-1,:], simulation)/m
         vel[i,:] = vel[i-1,:] + a*δt
         δx = 0.5*a*δt^2 #+ vel[i-1,:]*δt
         path[i,:] = path[i-1,:] + δx
@@ -22,7 +44,7 @@ function driftcharge!(path, charge)
     return vel
 end
 
-function signal(path, charge)
+function signal(path, charge, simulation)
     if charge == "h+"
         q = 1
     end
@@ -36,7 +58,7 @@ function signal(path, charge)
     m = 0
     rep = 0
     for i in 2:length(Δϕ_h)
-        Δϕ_h[i] = getWpot(path[i,:])-getWpot(path[i-1,:])
+        Δϕ_h[i] = getWpot(path[i,:], simulation)-getWpot(path[i-1,:], simulation)
         Q[i] = Q[i-1] + q*Δϕ_h[i]
         if Q[i] == Q[i-1]
             rep += 1
